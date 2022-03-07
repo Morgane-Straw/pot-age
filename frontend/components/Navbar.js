883fb2a56a615546004ca68ff23ab9872771cfaf
jacobgui1u <xavier.jacob-guizon1@etu.univ-lorraine.fr>
2022-03-07 17:29:50 +0100

    fix docker build et fix nextjs, remove default__app.js

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,25 +1,29 @@
 import NavbarButton from "./NavbarButton";
 import Link from "next/link";
+import Image from "next/image"
+
 
 export default function Navbar() {
     return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light nav nav-pills nav-fill">
-      <a className="navbar-brand nav-item" href="#"><img src='/img/LogoBleu.svg' alt="Pot'Age" height="32"></img></a>
-      <Link href="/proposer-evenement">
+      <a className="navbar-brand nav-item" href="#">
+          <Image src="/img/LogoBleu.svg" alt="Pot'Age" height="32" layout="fill"/>
+     </a>
+      <Link href="/proposer-evenement" passHref>
         <button className="nav-item btn-primary">
         <i className="far fa-plus-square "></i>
         <span className="">Proposer un évènement</span>
         </button>
       </Link>
-      <NavbarButton title="Rechercher un évènement" href ="/mes-evenements" icon="fas fa-search"></NavbarButton> 
-      <NavbarButton title="Mes évenements" href="/mes-evenements" icon="fas fa-flag"></NavbarButton>    
-      <NavbarButton title="Favoris" href="/en-chantier" icon="fas fa-heart"></NavbarButton>    
-      <NavbarButton title="Actualités" href="/actualites" icon="fas fa-book-open"></NavbarButton>    
-      <NavbarButton title="Messages" href="/en-chantier" icon="fas fa-envelope"></NavbarButton>    
+      <NavbarButton title="Rechercher un évènement" href ="/mes-evenements" icon="fas fa-search"></NavbarButton>
+      <NavbarButton title="Mes évenements" href="/mes-evenements" icon="fas fa-flag"></NavbarButton>
+      <NavbarButton title="Favoris" href="/en-chantier" icon="fas fa-heart"></NavbarButton>
+      <NavbarButton title="Actualités" href="/actualites" icon="fas fa-book-open"></NavbarButton>
+      <NavbarButton title="Messages" href="/en-chantier" icon="fas fa-envelope"></NavbarButton>
       <Link href='/mon-compte'>
             <a className='nav-item'>
-                <img></img>
-                
+               // <Image src="/Todo" alt="TODO" layout="fill"/>
+
                 <span>Mon compte</span>
             </a>
         </Link>
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout'
 import Head from 'next/head'
 import '../src/global.css'
+import './globals.css'
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 import '@fortawesome/fontawesome-free/css/brands.css';
 import '@fortawesome/fontawesome-free/css/solid.css';
@@ -9,7 +10,7 @@ import '@fortawesome/fontawesome-free/css/regular.css';
 export default function MyApp({ Component, pageProps }) {
   return (<>
     <Head>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
     </Head>
     <Layout>
       <Component {...pageProps} />

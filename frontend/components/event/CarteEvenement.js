85dd26fdd6921495592e1c0cb50a4f9625f6368d
Morgane-Straw <paillermorgane@gmail.com>
2022-03-27 17:05:34 +0200

    carte-evenement (commit julie billon)

---
+++ frontend/components/event/CarteEvenement.js
@@ -0,0 +1,28 @@
+export default function CarteEvenement() {
+    return (
+        <div class="card">
+            <div class="card-image"><img src="/uploads/apple-pie-g2708a1c90_1920.jpg" width="300" height="200" alt="Illustration garçon jouant au basketball" /></div>
+
+            <div class="card-body">
+
+                <div class="card-name">
+                    <h2>Basketball</h2>
+                </div>
+
+                <div class="card-description">
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aofficiis! Aniectetur unde autem inventore.</p>
+                </div>
+<div className="">
+                <div class="card-button2 rounded-1 bg-primary text-white">
+                    <button className="button1"><i src="/img/coeur.png" height="17" width="17" />Ajouter aux favoris</button>
+                </div>
+                <div class="card-button1">
+                    <button className="button1" type="submit">Consulter</button>
+                </div>
+</div>
+
+            </div>
+        </div>
+
+  )
+}
